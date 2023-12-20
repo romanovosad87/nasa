dff29e1cde2e57eea659c3e93ccac59316415280
Roman Novosad <roman.novosad@yahoo.com>
2023-12-20 08:11:00 +0100

    deleted useless @Transactional in service

--- src/main/java/com/example/nasastealphoto/service/CameraService.java
+++ src/main/java/com/example/nasastealphoto/service/CameraService.java
@@ -4,7 +4,6 @@ import com.example.nasastealphoto.model.Camera;
 import com.example.nasastealphoto.repository.CameraRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -12,7 +11,6 @@ public class CameraService {
 
     private final CameraRepository cameraRepository;
 
-    @Transactional
     public Camera saveCamera(Camera camera) {
         return cameraRepository.save(camera);
     }
